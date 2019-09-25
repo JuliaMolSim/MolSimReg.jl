c628512bb17f36569d81cdfec6bc71b26da0ee72
Christoph Ortner <c.ortner@warwick.ac.uk>
2019-09-25 09:21:25 +0100

    initial basic functionality

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LibAtomsReg
+
+Very rudimentary management utility for the [`LibAtoms` Julia registry](https://github.com/cortner/LibAtoms).
--- src/LibAtomsReg.jl
+++ src/LibAtomsReg.jl
@@ -1,5 +1,37 @@
 module LibAtomsReg
 
-greet() = print("Hello World!")
+import Registrator
+
+const _registered_modules =
+   [
+      "JuLIP",
+      "SHIPs",
+      "SKTB",
+      "JuLIPMaterials",
+      "NBodyIPs",
+      "IPFitting"
+   ]
+
+regpath() = joinpath(homedir(), ".julia", "registries", "LibAtoms")
+
+"""
+register the module `M` in the `LibAtoms` registry
+"""
+function register(M::Module; force = false, push = false)
+   if !string(M) in _registered_modules && !force
+      error("The module $(string(M)) appears not to be registered yet.")
+   end
+   Registrator.register(M, regpath())
+   if push
+      push!()
+   end
+end
+
+"""
+push the registry to the remote git repository
+"""
+function push!()
+   run(`cd $(regpath()); git push`)
+end
 
 end # module
