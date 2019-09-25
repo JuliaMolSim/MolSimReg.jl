d22e0edd1fabe79bc141bb05447ce3d1276da558
Christoph Ortner <c.ortner@warwick.ac.uk>
2019-09-25 09:12:17 +0100

    Files generated by PkgTemplates

---
+++ src/LibAtomsReg.jl
@@ -0,0 +1,5 @@
+module LibAtomsReg
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using LibAtomsReg
+using Test
+
+@testset "LibAtomsReg.jl" begin
+    # Write your own tests here.
+end
